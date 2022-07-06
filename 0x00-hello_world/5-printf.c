f9bcedb32d8fbaa2a27e5f422ea9716ecaeab176
codelord-evans <ekioko404@gmail.com>
2022-07-06 16:11:03 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,23 +3,11 @@
 
 
 /**
- *
- * * main - Prints "with proper grammar, but the ouptu is a piece of art,"
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
-
-
+ *main - prints the size of various types
+ *Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
